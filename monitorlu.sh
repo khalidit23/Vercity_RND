1a1da376df162bf788a8bc5d0b557d04eb088fae
khalidit23 <khalidcse039@gmail.com>
2023-10-16 17:06:15 +0600

    change script

--- monitorlu.sh
+++ monitorlu.sh
@@ -4,12 +4,12 @@ if [ "$EUID" -eq 0 ]; then
   echo "Exiting root mode..."
   target_user="admin"
   
-  su -l "$target_user" -c "gnome-screensaver-command -l"
+  su -l "$target_user"
+  gnome-screensaver-command -l
+
+  sleep 10
+
+  gnome-screensaver-command -d
   exit
 fi
 
-gnome-screensaver-command -l
-
-sleep 10
-
-gnome-screensaver-command -d
