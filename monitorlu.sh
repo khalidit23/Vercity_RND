2646b915acd1537fc0d644406aa068671c76eb11
khalidit23 <khalidcse039@gmail.com>
2023-10-17 16:56:48 +0600

    change script

--- monitorlu.sh
+++ monitorlu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo -u admin123 DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" gnome-screensaver-command -l
+sudo -u admin123 DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" gnome-screensaver-command -d
 
 sleep 10
 
-sudo -u admin123 DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" gnome-screensaver-command -d
+sudo -u admin123 DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" gnome-screensaver-command -l
